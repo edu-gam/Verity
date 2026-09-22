9a6364132e1be584ec94c0afc83d273b03a0c38d
edu-gam <edu.gam.04@gmail.com>
2026-09-22 17:41:41 -0500

    menu hecho

--- main.py
+++ main.py
@@ -4,6 +4,39 @@
 import json
 from funciones import *
 
-registrar_items()
-listar_items()
 
+while True:
+    print("=========================================")
+    print("   BIBLIOSTOCK - BIBLIOTECA HORIZONTE")
+    print("=========================================")
+    print("1. Registrar ítem")
+    print("2. Listar ítems")
+    print("3. Buscar ítem")
+    print("4. Registrar préstamo")
+    print("5. Registrar devolución")
+    print("6. Salir")
+    print("==========================================")
+
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        registrar_items()
+
+    elif opcion == "2":
+        listar_items()
+
+    elif opcion == "3":
+        print("Buscar ítem")
+
+    elif opcion == "4":
+        print("Registrar préstamo")
+
+    elif opcion == "5":
+        print("Registrar devolución")
+
+    elif opcion == "6":
+        print("Saliendo del programa...")
+        break
+
+    else:
+        print("Opción no válida")
